cac6c2f2633567ea021dc88699b3f3ecd1b87b0d
dovmial <nma91@mail.ru>
2017-06-11 23:42:49 +0500

    add Cpp's

--- ConsoleApplication1/ConsoleApplication3/Test3.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using std::cout;
-int main()
-{
-	cout << "Test3 welc!!2\n";
-	return 0;
-}
--- ConsoleApplication1/ConsoleApplication4/Test4.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using std::cout;
-int main()
-{
-	cout << "Test4 welc!!2\n";
-	return 0;
-}
