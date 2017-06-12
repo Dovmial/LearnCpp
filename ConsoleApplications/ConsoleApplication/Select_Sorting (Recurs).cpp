791e1bc9f2801551b9c974d9c4ddc835dad513af
dovmial <nma91@mail.ru>
2017-06-12 21:27:53 +0500

    добавление комментариев в сортировку

--- ConsoleApplications/ConsoleApplication/Select_Sorting (Recurs).cpp
+++ ConsoleApplications/ConsoleApplication/Select_Sorting (Recurs).cpp
@@ -6,23 +6,27 @@ const int ArrSize = 12;
 void selectionSort(int[], int);
 int main()
 {
-
+	//тестовый массив
 	int arr1[ArrSize] = { 12,13,3,4,0,123,8,9,1,26,6 };
-
+	//вывод на печать массива
 	for (int i = 0; i < ArrSize; i++)
 		cout << arr1[i] << " ";
 	cout << endl << "After Sorting:\n";
-	selectionSort(arr1, ArrSize);
+	selectionSort(arr1, ArrSize);     //сортировка рекурсивная выборкой
+	//печать массива после сортировки
 	for (int i = 0; i < ArrSize; i++)
 		cout << arr1[i] << " ";
 	cout << endl;
 	return 0;
 }
+//метод сортировки
 void selectionSort(int arr2[], int arrSz)
 {
-	int min; int y;
+	int min; 
+	int y; //индекс минимального значения
 	min = arr2[ArrSize - arrSz];
 	y = ArrSize - arrSz;
+	//поиск минимального значения в массиве и его местоположения
 	for (int i = ArrSize - arrSz + 1; i < ArrSize; i++)
 	{
 		if (min > arr2[i])
@@ -31,12 +35,14 @@ void selectionSort(int arr2[], int arrSz)
 			y = i;
 		}
 	}
+	//постановка минимального значения на свое место(обмен местами)
 	if (arr2[ArrSize - arrSz] != arr2[y])
 	{
 		min = arr2[ArrSize - arrSz];
 		arr2[ArrSize - arrSz] = arr2[y];
 		arr2[y] = min;
 	}
-	if (arrSz != 1)
+	//рекурсивный вызов функции для подмассива
+	if (arrSz != 1)   
 		selectionSort(arr2, arrSz - 1);
 }
